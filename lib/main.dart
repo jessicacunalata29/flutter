02cda3d916092f40dcca0899f70da53f25d866a6
jessicacunalata29 <jessicacunalata29@gmail.com>
2025-11-19 22:14:08 -0500

    Conectado

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ejemplo/src/app.dart';
 import 'package:ejemplo/src/inicio.dart';
-import 'src/state/app_state.dart'; // Nuevo archivo que contiene AppState
+import 'src/state/app_state.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(create: (_) => AppState(), child: const MyApp()),
-);
+// Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializar Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Mensaje para verificar la conexión
+  print("🔥 Firebase inicializado correctamente");
+
+  runApp(
+    ChangeNotifierProvider(create: (_) => AppState(), child: const MyApp()),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,7 +37,7 @@ class MyApp extends StatelessWidget {
           home: const Homepage(),
           routes: {
             '/inicio': (context) => const Inicio(),
-            '/configuracion': (context) => const Configuracion(), // Nueva ruta
+            '/configuracion': (context) => const Configuracion(),
           },
         );
       },
